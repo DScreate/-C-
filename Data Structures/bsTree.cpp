01adc2545dc1fef4bb15dc5e83f896fef69809b6
Derek <derekasamsemail@gmail.com>
2019-05-25 18:26:03 -0700

    Basic Commit

---
+++ Data Structures/bsTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derek on 5/22/2019.
+//
+
+#include "bsTree.h"
---
+++ Data Structures/bsTree.h
@@ -0,0 +1,21 @@
+//
+// Created by derek on 5/22/2019.
+//
+
+#ifndef C_BSTREE_H
+#define C_BSTREE_H
+
+typedef struct bsTree<T> {
+    T data;
+    struct bsTree *parent;
+    struct bsTree *left;
+    struct bsTree *right;
+};
+
+
+class bsTree {
+
+};
+
+
+#endif //C_BSTREE_H
